a4795c3351d49884665930928b58b8a44614b3a7
AndreiDegtiarev <Andrey.Degtyarev@gmail.com>
2017-01-08 20:08:26 +0100

    Initial commit

---
+++ WebSharper.Community.JqPropertyGrid/Main.fs
@@ -0,0 +1,35 @@
+namespace ExtJqPropertyGrid
+
+open WebSharper
+open WebSharper.JavaScript
+open WebSharper.InterfaceGenerator
+
+module Definition =
+
+
+    let JqPropertyGrid = Class "JqPropertyGrid"
+                         |+> Static [
+                           "create" => T<Dom.Element>?el * T<obj>?theObj * T<obj>?theMeta ^-> T<unit> |>  WithInline  "jQuery($el).jqPropertyGrid($theObj,$theMeta)" 
+                         ] 
+
+    let Assembly =
+        Assembly [
+            Namespace "WebSharper.Community.JqPropertyGrid" [
+                 JqPropertyGrid
+            ]
+            Namespace "WebSharper.Community.JqPropertyGrid.Resources" [
+                Resource "jqueryui_" "//ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/jquery-ui.min.js"
+                Resource "Extension3" "/Scripts/jqPropertyGrid.js"
+                |> fun r -> r.AssemblyWide()
+            ]
+
+        ]
+
+[<Sealed>]
+type Extension() =
+    interface IExtension with
+        member ext.Assembly =
+            Definition.Assembly
+
+[<assembly: Extension(typeof<Extension>)>]
+do ()
